480ca9b9fe244ea84b71abf7e1926b4416017097
Oleg <luchkoo22@gmail.com>
2025-10-23 13:59:15 +0300

    task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -1,10 +1,14 @@
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+}
+
+ console.log(getFirstElement<number>([1, 2, 3]));           // 1
+ console.log(getFirstElement<string>(["a", "b", "c"]));     // "a"
+ console.log(getFirstElement<boolean>([true, false, true])); // true
+
 // Задача 6. Узагальнені типи (файл task-6.ts)
 
-
-
 // Функція getFirstElement приймає масив і повертає його перший елемент.
-
-
 
 // function getFirstElement(arr) {
 //   return arr[0];
@@ -13,8 +17,6 @@
 // getFirstElement([1, 2, 3]);           // 1
 // getFirstElement(["a", "b", "c"]);     // "a"
 // getFirstElement([true, false, true]); // true
-
-
 
 // Завдання:
 
